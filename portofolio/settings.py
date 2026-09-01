d24d827fa072b0d53b7f5308edc813ef3edcc625
michellearitonang <michellearitonang16@gmail.com>
2026-09-01 21:27:13 +0700

    Fix staticfiles directory path

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'main' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 WHITENOISE_USE_FINDERS = True
 
