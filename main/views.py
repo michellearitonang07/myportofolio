ec1279c8ddded2f0e80d61203987452e3c014855
michellearitonang <michellearitonang16@gmail.com>
2026-09-09 17:27:52 +0700

    Selesaikan Tutorial 02 - Implementasi MVT

--- main/views.py
+++ main/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from main.models import Experience
 
-def index(request):
-    return render(request, 'index.html')
+def show_main(request):
+    context = {
+        "name": "Michelle Yuyun Margarethy Aritonang",
+        "npm": "2506656961",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Mahasiswi Sistem Informasi Universitas Indonesia yang tertarik "
+            "pada pengembangan perangkat lunak, organisasi, dan desain."
+        ),
+    }
+    return render(request, "index.html", context)
+
+def show_experience(request):
+    context = {
+        "name": "Michelle Yuyun Margarethy Aritonang",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
